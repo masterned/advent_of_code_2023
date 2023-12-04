de3e464c56145938e30145c94bd9a88b5bba7454
Spencer Dent <mr.spencerdent@gmail.com>
2023-12-04 15:50:21 -0500

    feat: solve day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,8 +1,8 @@
 use regex::Regex;
-use std::{error::Error, fmt, fs, str::FromStr};
+use std::{collections::HashSet, error::Error, fmt, fs, str::FromStr};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Part {
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct PartNumber {
     pub number: usize,
     pub line: usize,
     pub start: usize,
@@ -10,13 +10,20 @@ struct Part {
 }
 
 #[derive(Clone, Debug)]
+struct Gear {
+    coord: (usize, usize),
+    ratios: HashSet<PartNumber>,
+}
+
+#[derive(Clone, Debug)]
 struct Schematic {
     cells: Vec<Vec<Option<char>>>,
-    parts: Vec<Part>,
+    parts: Vec<PartNumber>,
+    gears: Vec<Gear>,
 }
 
 impl Schematic {
-    fn is_valid_part(&self, part: &Part) -> bool {
+    fn is_valid_part_number(&self, part: &PartNumber) -> bool {
         let mut range_start = part.start;
         if part.start > 0 {
             range_start = part.start - 1;
@@ -56,11 +63,62 @@ impl Schematic {
         false
     }
 
-    fn get_valid_parts(&self) -> Vec<&Part> {
+    fn get_valid_part_numbers(&self) -> Vec<&PartNumber> {
         self.parts
             .iter()
-            .filter(|part| self.is_valid_part(part))
+            .filter(|part| self.is_valid_part_number(part))
             .collect()
+    }
+
+    fn get_full_part_number(&self, x: usize, y: usize) -> Option<&PartNumber> {
+        self.parts
+            .iter()
+            .filter(|part| part.line == y)
+            .filter(|part| part.start <= x && part.end > x)
+            .collect::<Vec<&PartNumber>>()
+            .first()
+            .copied()
+    }
+
+    fn populate_gear_ratios(&mut self) {
+        let updated_gears = self
+            .gears
+            .iter()
+            .map(|gear| {
+                let (x, y) = gear.coord;
+
+                let mut ratios = HashSet::new();
+
+                [
+                    (0, -1),  // North
+                    (1, -1),  // North-East
+                    (1, 0),   // East
+                    (1, 1),   // South-East
+                    (0, 1),   // South
+                    (-1, 1),  // South-West
+                    (-1, 0),  // West
+                    (-1, -1), // North-West
+                ]
+                .iter()
+                .for_each(|(x_d, y_d)| {
+                    if let Some(x_n) = x.checked_add_signed(*x_d) {
+                        if let Some(y_n) = y.checked_add_signed(*y_d) {
+                            if let Some(full_number) = self.get_full_part_number(x_n, y_n) {
+                                ratios.insert(full_number);
+                            }
+                        }
+                    }
+                });
+
+                let ratios = ratios.iter().copied().cloned().collect();
+
+                Gear {
+                    ratios,
+                    coord: (x, y),
+                }
+            })
+            .collect();
+        self.gears = updated_gears;
     }
 }
 
@@ -80,39 +138,51 @@ impl FromStr for Schematic {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parts = Vec::new();
+        let mut gears = Vec::new();
 
         let cells = s
             .lines()
             .enumerate()
-            .map(|(i, l)| {
+            .map(|(y, line)| {
                 let re = Regex::new(r"\d+").unwrap();
-                re.find_iter(l).for_each(|cap| {
-                    let part = Part {
+                re.find_iter(line).for_each(|cap| {
+                    let part = PartNumber {
                         number: cap.as_str().parse().unwrap(),
-                        line: i,
+                        line: y,
                         start: cap.start(),
                         end: cap.end(),
                     };
                     parts.push(part);
                 });
 
-                let mut line_width = 0;
-
-                let cells = l
+                let cells = line
                     .chars()
-                    .map(|c| {
-                        line_width += 1;
-                        match c {
-                            '.' => None,
-                            x => Some(x),
+                    .enumerate()
+                    .map(|(x, c)| match c {
+                        '.' => None,
+                        '*' => {
+                            gears.push(Gear {
+                                coord: (x, y),
+                                ratios: HashSet::new(),
+                            });
+                            Some('*')
                         }
+                        c => Some(c),
                     })
                     .collect();
 
                 cells
             })
             .collect();
-        Ok(Schematic { cells, parts })
+
+        let mut schematic = Schematic {
+            cells,
+            parts,
+            gears,
+        };
+        schematic.populate_gear_ratios();
+
+        Ok(schematic)
     }
 }
 
@@ -122,11 +192,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let scheme: Schematic = input.parse()?;
 
     let valid_part_number_sum: usize = scheme
-        .get_valid_parts()
+        .get_valid_part_numbers()
         .iter()
         .map(|part| part.number)
         .sum();
     println!("Part 1: {valid_part_number_sum:?}");
+
+    let valid_gears = scheme
+        .gears
+        .iter()
+        .filter(|gear| gear.ratios.len() == 2)
+        .map(|gear| gear.ratios.iter().map(|n| n.number).product::<usize>())
+        .sum::<usize>();
+    println!("Part 2: {valid_gears:?}");
 
     Ok(())
 }
@@ -151,56 +229,56 @@ mod tests {
         let schematic: Schematic = EXAMPLE_INPUT.parse().unwrap();
         assert_eq!(
             vec![
-                &Part {
+                &PartNumber {
                     number: 467,
                     line: 0,
                     start: 0,
                     end: 3
                 },
-                &Part {
+                &PartNumber {
                     number: 35,
                     line: 2,
                     start: 2,
                     end: 4
                 },
-                &Part {
+                &PartNumber {
                     number: 633,
                     line: 2,
                     start: 6,
                     end: 9
                 },
-                &Part {
+                &PartNumber {
                     number: 617,
                     line: 4,
                     start: 0,
                     end: 3,
                 },
-                &Part {
+                &PartNumber {
                     number: 592,
                     line: 6,
                     start: 2,
                     end: 5,
                 },
-                &Part {
+                &PartNumber {
                     number: 755,
                     line: 7,
                     start: 6,
                     end: 9,
                 },
-                &Part {
+                &PartNumber {
                     number: 664,
                     line: 9,
                     start: 1,
                     end: 4,
                 },
-                &Part {
+                &PartNumber {
                     number: 598,
                     line: 9,
                     start: 5,
                     end: 8
                 }
             ],
-            schematic.get_valid_parts()
+            schematic.get_valid_part_numbers()
         );
     }
 }
