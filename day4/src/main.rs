ff040809851bc60a469113add43eef447a01fd0c
Spencer Dent <mr.spencerdent@gmail.com>
2023-12-05 23:06:47 -0500

    Clean-up day 4 part 1 & attempt part 2
    My part 2 code is working on the example, but something about it is
    causing an integer overflow -- even when using `u128`...

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,33 +1,130 @@
-use std::{error::Error, fs};
+#![warn(clippy::pedantic)]
+
+use std::{
+    error::Error,
+    fmt, fs,
+    str::{FromStr, Split},
+};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Card {
+    pub number: usize,
+    pub count: usize,
+    win_numbers: Vec<String>,
+    player_numbers: Vec<String>,
+    pub win_count: usize,
+}
+
+impl Card {
+    pub fn count_wins(&self) -> usize {
+        self.player_numbers
+            .iter()
+            .filter(|n| self.win_numbers.contains(n))
+            .count()
+    }
+
+    pub fn calculate_score(count: usize) -> usize {
+        if count > 0 {
+            1 << (count - 1)
+        } else {
+            0
+        }
+    }
+}
+
+impl Default for Card {
+    fn default() -> Self {
+        Card {
+            number: 0,
+            count: 1,
+            win_numbers: vec![],
+            player_numbers: vec![],
+            win_count: 0,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct ParseCardError();
+
+impl fmt::Display for ParseCardError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Unable to parse card")
+    }
+}
+
+impl Error for ParseCardError {}
+
+impl FromStr for Card {
+    type Err = ParseCardError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(':');
+        let number = split
+            .next()
+            .ok_or(ParseCardError())?
+            .split_whitespace()
+            .nth(1)
+            .ok_or(ParseCardError())?
+            .parse::<usize>()
+            .map_err(|_| ParseCardError())?;
+
+        let mut split = split.next().ok_or(ParseCardError())?.split('|');
+
+        let split_num_seq = |split: &mut Split<'_, char>| {
+            Ok(split
+                .next()
+                .ok_or(ParseCardError())?
+                .split_whitespace()
+                .map(ToString::to_string)
+                .collect())
+        };
+
+        let win_numbers = split_num_seq(&mut split)?;
+
+        let player_numbers = split_num_seq(&mut split)?;
+
+        let mut card = Card {
+            number,
+            win_numbers,
+            player_numbers,
+            ..Default::default()
+        };
+        card.win_count = card.count_wins();
+
+        Ok(card)
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("data/day4")?;
 
-    let score_total: usize = input
+    let mut cards: Vec<_> = input
         .lines()
-        .map(|card| {
-            card.split(':').last().map_or(0, |content: &str| {
-                let mut split = content.split('|');
-                split.next().map_or(0, |winners_str: &str| {
-                    split.last().map_or(0, |my_numbers_str: &str| {
-                        let win_numbers: Vec<_> = winners_str.split_whitespace().collect();
-                        let my_numbers = my_numbers_str.split_whitespace();
+        .filter_map(|line| line.parse::<Card>().ok())
+        .collect();
 
-                        my_numbers.filter(|n| win_numbers.contains(n)).count()
-                    })
-                })
-            })
-        })
-        .map(|count| {
-            if count > 0 {
-                2_usize.pow((count - 1) as u32)
-            } else {
-                0
-            }
-        })
+    let win_counts: Vec<_> = cards.iter().map(|card| card.win_count).collect();
+
+    let score_total: usize = win_counts
+        .iter()
+        .map(|&count| Card::calculate_score(count))
         .sum();
-
     println!("Part 1: {score_total:?}");
+
+    let mut multiplier = 1;
+    for (i, win_count) in win_counts.iter().enumerate() {
+        for n in 0..*win_count {
+            if let Some(card) = cards.get_mut(i + n + 1) {
+                card.count += multiplier;
+            }
+        }
+        if let Some(card) = cards.get(i) {
+            multiplier += card.count;
+        }
+    }
+    let card_count = cards.iter().map(|card| card.count).sum::<usize>();
+    println!("Part 2: {card_count}");
 
     Ok(())
 }
